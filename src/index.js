e6acdab43060186345c7a877a021214194c5451e
Dinh <dinh.hashstack@gmail.com>
2021-12-01 09:48:21 +0800

    index

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { DAppProvider } from "@usedapp/core";
 ReactDOM.render(
   <React.StrictMode>
     <MoralisProvider appId="ymdgw0muLMW7cEiNA3eRatEjQAmdloRoObUrUemp" serverUrl="https://gwfyfk2dqlj8.usemoralis.com:2053/server">
-     <MoralisProvider appId="E8ZQf3R5j0DKNJS8gVk7Fy2Hs4ieUPd7bLvKyfSZ" serverUrl="https://ocxnapxizly3.usemoralis.com:2053/server">
+     {/* <MoralisProvider appId="E8ZQf3R5j0DKNJS8gVk7Fy2Hs4ieUPd7bLvKyfSZ" serverUrl="https://ocxnapxizly3.usemoralis.com:2053/server"> */}
        <MoralisDappProvider>
         <Web3ModalProvider>
           <Web3WrapperProvider>
