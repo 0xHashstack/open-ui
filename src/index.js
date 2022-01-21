b26d65ac65b5210cc5e267cf21ab5e16da4d5c36
Markish <markishh@gmail.com>
2022-01-21 08:54:48 +0530

    Sentry Addition

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import axios from "axios";
 import Dashboard from './pages/Dashboard';
 import HashstackCrypto from './pages/hashstack-crypto';
@@ -13,6 +15,12 @@ import "./assets/scss/theme.scss";
 import Layout from './components/layout';
 import { Provider } from "react-redux";
 import store from "./store";
+
+Sentry.init({
+  dsn: "https://ca35bb6a621b435cac53fee2d5cf5b4d@o1122653.ingest.sentry.io/6160093",
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
