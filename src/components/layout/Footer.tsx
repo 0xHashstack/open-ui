3bcc10fa41c8a5e4c51cf25650a26ed8448a02a8
Karthik-TK <karthiktk16@gmail.com>
2022-01-27 19:33:09 +0530

    Added typescript files for layout component

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import { Container, Row, Col } from "reactstrap"
+
+const Footer = () => {
+  return (
+    <React.Fragment>
+      <footer className="footer">
+        <Container fluid={true}>
+          <Row>
+            <Col md={6}>{new Date().getFullYear()} © Hashstack.</Col>
+            <Col md={6}>
+              <div className="text-sm-end d-none d-sm-block" data-testid="footer-text">
+                Design & Develop by Hashstack Finance
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+    </React.Fragment>
+  )
+}
+
+export default Footer
