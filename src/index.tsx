4f8fb0302bc7d98216079e33bbc8f7fec4d929c3
Pankaj Meshram <mpankajarun@gmail.com>
2022-02-08 19:03:15 +0530

    [BUG_FIX][HSH-121] Index converted to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
 import axios from "axios";
-import HashstackCrypto from './pages/hashstack-crypto.tsx';
+import HashstackCrypto from './pages/hashstack-crypto';
 import Web3ModalProvider from "contexts/Web3ModalProvider";
 import Web3WrapperProvider from "contexts/Web3WrapperProvider";
 import { MoralisProvider } from "react-moralis";
 import { MoralisDappProvider } from "./providers/MoralisDappProvider/MoralisDappProvider";
 import { BrowserRouter } from 'react-router-dom';
-import Layout from './components/layout/index.tsx';
+import Layout from './components/layout/index';
 import { Provider } from "react-redux";
 import store from "./store";
 import "./assets/scss/theme.scss";
