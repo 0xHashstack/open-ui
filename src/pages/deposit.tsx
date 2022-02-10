06011e45213f4a5230810d91bc009c51dc451add
Pankaj Meshram <mpankajarun@gmail.com>
2022-02-10 13:13:14 +0530

    [REFACTOR][HSH-121] Deposit Component separated

---
+++ src/pages/deposit.tsx
@@ -0,0 +1,146 @@
+import { useState, useEffect, useContext} from "react";
+import {
+  Col,
+  Button,
+  Form,
+  Input,
+  Modal
+} from "reactstrap";
+
+import {
+  EventMap,
+  SymbolsMap, DecimalsMap, DepositInterestRates, CommitMap, VariableDepositInterestRates
+} from '../blockchain/constants';
+import { Web3ModalContext } from '../contexts/Web3ModalProvider';
+import { Web3WrapperContext } from '../contexts/Web3WrapperProvider';
+import { BNtoNum, GetErrorText } from '../blockchain/utils';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+toast.configure({
+    autoClose: 4000
+});
+
+const Deposit = (props) => {
+
+    const [commitPeriod, setCommitPeriod] = useState();
+    const [modal_deposit, setmodal_deposit] = useState(false);
+
+  
+    let inputVal = 0;
+  
+    const { account } = useContext(Web3ModalContext);
+    const { web3Wrapper: wrapper } = useContext(Web3WrapperContext);
+
+    useEffect(() => {
+      wrapper?.getDepositInstance().deposit.on(EventMap.NEW_DEPOSIT, onDeposit);
+      wrapper?.getDepositInstance().deposit.on(EventMap.DEPOSIT_ADDED, onDeposit);
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const handleDepositChange = (e) => {
+      setCommitPeriod(e.target.value)
+    }
+
+    function removeBodyCss() {
+        document.body.classList.add("no_padding");
+      }
+      function tog_center() {
+        setmodal_deposit(!modal_deposit);
+        removeBodyCss();
+      }
+
+    const handleDeposit = async () => {
+      try {
+        const approveTransactionHash = await wrapper?.getMockBep20Instance().approve(SymbolsMap[props.asset], inputVal, DecimalsMap[props.asset]);
+        console.log("Approve Transaction sent: ", approveTransactionHash);
+        const _commitPeriod: string | undefined =  commitPeriod;
+        await wrapper?.getDepositInstance().depositRequest(SymbolsMap[props.asset], CommitMap[_commitPeriod], inputVal, DecimalsMap[props.asset]);
+      } catch (err) {
+        if (err instanceof Error) {
+          toast.error(`${GetErrorText(String(err.message))}`, { position: toast.POSITION.BOTTOM_RIGHT, closeOnClick: true});
+        } else {
+          toast.error(`${GetErrorText(String(err))}`, { position: toast.POSITION.BOTTOM_RIGHT, closeOnClick: true});
+        }
+      }
+    }
+
+    const onDeposit = (data) => {
+      let amount = BNtoNum(Number(data.amount),DecimalsMap[data.market]);
+      toast.success(`Deposited amount: ${amount}`, { position: toast.POSITION.BOTTOM_RIGHT, closeOnClick: true});
+    }
+
+
+    return (
+      <>
+        <button
+          type="button"
+          className="btn btn-dark btn-sm w-xs"
+          onClick={() => {
+            tog_center();
+          }}
+        >
+          Deposit
+        </button>
+        <Modal
+          isOpen={modal_deposit}
+          toggle={() => {
+            tog_center();
+          }}
+          centered
+        >
+          <div className="modal-body">
+            {account ?
+              <Form>
+                <div className="row mb-4">
+                  <h6>{props.asset}</h6>
+                </div>
+                <div className="row mb-4">
+                  <Col sm={12}>
+                    <Input
+                      type="number"
+                      className="form-control"
+                      id="amount"
+                      placeholder="Amount"
+                      onChange={(event) => { inputVal = Number(event.target.value) }}
+                    />
+                  </Col>
+                </div>
+                <div className="row mb-4">
+                  <Col sm={12}>
+                    <select className="form-select" placeholder="Commitment" onChange={handleDepositChange}>
+                      <option hidden>Commitment</option>
+                      <option value={"NONE"}>None</option>
+                      <option value={"TWOWEEKS"}>Two Weeks</option>
+                      <option value={"ONEMONTH"}>One Month</option>
+                      <option value={"THREEMONTHS"}>Three Months</option>
+                    </select>
+                  </Col>
+                </div>
+                <div className="row mb-4">
+                  <Col sm={6}>
+                    <p>Fixed APY <strong>{DepositInterestRates[commitPeriod || "NONE"] || "7.8%"}</strong></p>
+                  </Col>
+                  <Col sm={6}>
+                    <p style={{ float: "right" }}>Variable APY <strong>{VariableDepositInterestRates[commitPeriod || "NONE"] || "0%"}</strong></p>
+                  </Col>
+                </div>
+                <div className="d-grid gap-2">
+                  <Button
+                    color="primary"
+                    className="w-md"
+                    disabled={commitPeriod === undefined}
+                    onClick={handleDeposit}
+                  >
+                    Deposit
+                  </Button>
+                </div>
+              </Form>
+              : <h2>Please connect your wallet</h2>}
+          </div>
+        </Modal>
+      </>
+    )
+  }
+
+  export default Deposit;
