36d7c120a762c7404ec3adba4dca75c7cb92a7da
ShubhamChndrvnshi <shubham.chndrvnshi@gmail.com>
2022-02-21 21:26:53 +0530

    ENHANCEMENT | Banner component | IN_PROGRESS

---
+++ src/components/banner/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function index() {
+  return (
+    <div style={{textAlign: "center"}}>&quot;These features will be activated this Friday - swapLoan &amp; swapToLaon, repayLaon, withdrawCollateral&quot;</div>
+  )
+}
