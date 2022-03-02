cf55686b6c2712f885481fee8f72e9bf77f75611
nobita851 <rktipu1995@gmail.com>
2022-03-02 22:05:43 +0530

    Testing Ethers Implementation

--- src/test/deposit.test.ts
+++ src/test/deposit.test.ts
@@ -3,6 +3,7 @@ import { providers } from "ethers"
 import Web3Wrapper from "../blockchain/web3/Web3Wrapper"
 import { assert } from "chai"
 import { BNtoNum } from "../blockchain/utils"
+import { CommitMap, DecimalsMap, SymbolsMap } from "../blockchain/constants";
 
 describe("Tests", () => {
   let 
@@ -10,13 +11,16 @@ describe("Tests", () => {
     ethersInstance, 
     signer, 
     web3Wrapper,
-    testTimeOut = 30000;
+    testTimeOut = 60000;
 
   beforeEach(async ()=>{
     provider = new HDWalletProvider({
-      privateKeys: ["d631de5b7e9cf451135896c833187c8b4dc230bf47756a9a2ca4ffccc161175e"],
-      providerOrUrl: "https://nd-400-266-190.p2pify.com/1efac602169fba8d5bf0589315ec436a"
-    });
+      privateKeys: [
+        "b2555fe5399c2da6d7b9dbcbb02729387348e154d6f91dc6c50f377f5a7daafc",
+      ],
+      providerOrUrl:
+        "https://speedy-nodes-nyc.moralis.io/a61a52180976b08f4ff81c83/bsc/testnet",
+    })
     ethersInstance = new providers.Web3Provider(provider)
     // await ethersInstance.send("eth_requestAccounts", [])
     signer = await ethersInstance.getSigner()
@@ -39,8 +43,8 @@ describe("Tests", () => {
     }
   }, testTimeOut)
 
-  describe("New Deposit", () => {
-    it("USDT Deposit", async () => {
+  describe("Deposit Functions", () => {
+    it("New USDT Deposit", async () => {
     const approveTransactionHash = await web3Wrapper
       ?.getMockBep20Instance()
       .approve("0x555344542e740000000000000000000000000000000000000000000000000000",
@@ -70,39 +74,204 @@ describe("Tests", () => {
       _amount = BNtoNum(_amount, 18)
       assert.equal(eventName, "NewDeposit", "Event Name Correct")
       assert.equal(_amount, 200, "Amount Correct")
-    },testTimeOut)      
-  })
-  describe("Add to Deposit", () => {
-    it("USDT Deposit", async () => {
-      const approveTransactionHash = await web3Wrapper
-        ?.getMockBep20Instance()
-        .approve(
-          "0x555344542e740000000000000000000000000000000000000000000000000000",
-          200,
-          18
-        )
-      await approveTransactionHash.wait()
-      const tx1 = await web3Wrapper
-        ?.getDepositInstance()
-        .depositRequest(
-          "0x555344542e740000000000000000000000000000000000000000000000000000",
-          "0x636f6d69745f4e4f4e4500000000000000000000000000000000000000000000",
-          200,
-          18
-        )
-      const tx = await tx1.wait()
+    },testTimeOut) 
+    it(
+      "Add to USDT Deposit",
+      async () => {
+        const approveTransactionHash = await web3Wrapper
+          ?.getMockBep20Instance()
+          .approve(
+            "0x555344542e740000000000000000000000000000000000000000000000000000",
+            200,
+            18
+          )
+        await approveTransactionHash.wait()
+        const tx1 = await web3Wrapper
+          ?.getDepositInstance()
+          .depositRequest(
+            "0x555344542e740000000000000000000000000000000000000000000000000000",
+            "0x636f6d69745f4e4f4e4500000000000000000000000000000000000000000000",
+            200,
+            18
+          )
+        const tx = await tx1.wait()
 
-      let eventName
-      let _amount
-      tx.events.forEach(e => {
-        if (e.event == "DepositAdded") {
-          eventName = e.event
-          _amount = e.args.amount.toBigInt()
+        let eventName
+        let _amount
+        tx.events.forEach(e => {
+          if (e.event == "DepositAdded") {
+            eventName = e.event
+            _amount = e.args.amount.toBigInt()
+          }
+        })
+        _amount = BNtoNum(_amount, 18)
+        assert.equal(eventName, "DepositAdded", "Event Name Correct")
+        assert.equal(_amount, 200, "Amount Correct")
+      },
+      testTimeOut
+    )   
+    it(
+      "Withdraw Deposit ",
+      async () => {
+        try {
+          const tx1 = await web3Wrapper?.getDepositInstance().withdrawDeposit(
+            SymbolsMap["USDT"],
+            CommitMap["NONE"],
+            100,
+            DecimalsMap["USDT"]
+          )
+          const tx = await tx1.wait()
+          let eventName
+          tx.events.forEach(e => {
+            if (e.event == "DepositWithdrawal") {
+              eventName = e.event
+            }
+          })
+          assert.equal(eventName, "DepositWithdrawal")
+        } catch (e: any) {
+          console.error(e)
+          assert.equal(
+            e.error.message,
+            "execution reverted: ERROR: Insufficient balance"
+          )
         }
-      })
-      _amount = BNtoNum(_amount, 18)
-      assert.equal(eventName, "DepositAdded", "Event Name Correct")
-      assert.equal(_amount, 200, "Amount Correct")
-    },testTimeOut)
+      },
+      testTimeOut
+    )
+    
+  })
+  describe("Loan Functions", () => {
+    it(
+      "Borrow",
+      async () => {
+        try {
+          const approveTransactionHash = await web3Wrapper
+            ?.getMockBep20Instance()
+            .approve(
+              "0x555344542e740000000000000000000000000000000000000000000000000000",
+              200,
+              18
+            )
+          await approveTransactionHash.wait()
+          const tx1 = await web3Wrapper?.getLoanInstance().loanRequest(
+            SymbolsMap["USDT"],
+            CommitMap["NONE"],
+            300,
+            DecimalsMap["USDT"],
+            SymbolsMap["USDT"],
+            200,
+            DecimalsMap["USDT"]
+          )
+          const tx = await tx1.wait()
+          let eventName
+          tx.events.forEach(e => {
+            if (e.event == "NewLoan") {
+              eventName = e.event
+            }
+          })
+          assert.equal(eventName, "NewLoan")
+        } catch (e: any) {
+          assert.equal(
+            e.error.message,
+            "execution reverted: ERROR: Active loan"
+          )
+        }
+      },
+      testTimeOut
+    )
+
+    it(
+      "Add Collateral",
+      async () => {
+          const approveTransactionHash = await web3Wrapper
+            ?.getMockBep20Instance()
+            .approve(
+              "0x555344542e740000000000000000000000000000000000000000000000000000",
+              200,
+              18
+            )
+          await approveTransactionHash.wait()
+          const tx1 = await web3Wrapper?.getLoanInstance().addCollateral(
+            SymbolsMap["USDT"],
+            CommitMap["NONE"],
+            200,
+            DecimalsMap["USDT"]
+          )
+          const tx = await tx1.wait()
+          let eventName
+          tx.events.forEach(e => {
+            if (e.event == "AddCollateral") {
+              eventName = e.event
+            }
+          })
+          assert.equal(eventName, "AddCollateral")
+        
+      },
+      testTimeOut
+    )
+
+    it(
+      "Withdraw Partial Loan",
+      async () => {
+        try {
+          const tx1 = await web3Wrapper?.getLoanInstance().permissibleWithdrawal(
+            SymbolsMap["USDT"],
+            CommitMap["NONE"],
+            50,
+            DecimalsMap["USDT"]
+          )
+          const tx = await tx1.wait()
+          let eventName
+          tx.events.forEach(e => {
+            if (e.event == "WithdrawPartialLoan") {
+              eventName = e.event
+            }
+          })
+          assert.equal(eventName, "WithdrawPartialLoan")
+        } catch (e: any) {
+          console.error(e)
+          assert.equal(
+            e.error.message,
+            "execution reverted: ERROR: Request exceeds funds"
+          )
+        }
+      },
+      testTimeOut
+    )
+
+
+    it(
+      "Repay Loan",
+      async () => {
+        try {
+          const approveTransactionHash = await web3Wrapper
+            ?.getMockBep20Instance()
+            .approve(
+              "0x555344542e740000000000000000000000000000000000000000000000000000",
+              300,
+              18
+            )
+          await approveTransactionHash.wait()
+          const tx1 = await web3Wrapper?.getLoanInstance().loanRequest(
+            SymbolsMap["USDT"],
+            CommitMap["NONE"],
+            300,
+            DecimalsMap["USDT"]
+          )
+          const tx = await tx1.wait()
+          let eventName
+          tx.events.forEach(e => {
+            if (e.event == "LoanRepaid") {
+              eventName = e.event
+            }
+          })
+          assert.equal(eventName, "LoanRepaid")
+        } catch (e: any) {
+          console.error(e)
+          assert.equal(e.error.message, "execution reverted: ERROR: No Loan")
+        }
+      },
+      testTimeOut
+    )
   })
 })
