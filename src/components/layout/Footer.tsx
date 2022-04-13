21cfc73af7c93f6bb2adb8b98c7fd05157d7c921
nobita851 <rktipu1995@gmail.com>
2022-04-13 10:58:45 +0530

    Fixed Reddit Link

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Container, Row, Col } from "reactstrap"
-import './Footer.scss';
+import "./Footer.scss"
 const Footer = () => {
   return (
     <React.Fragment>
@@ -10,12 +10,62 @@ const Footer = () => {
             <Col md={6}>{new Date().getFullYear()} © Hashstack.</Col>
             <Col md={6}>
               <div className="text-sm-end w-layout-grid footer-socials">
-                <a href="https://twitter.com/0xhashstack" rel="noreferrer" target="_blank" className="w-inline-block"><img src="./images/Twitter-Icon-Fill.svg" loading="lazy" alt="" /></a>
-                <a href="https://in.linkedin.com/company/0xhashstack" rel="noreferrer" target="_blank" className="w-inline-block"><img src="./images/Linkedin-Icon-Fill.svg" loading="lazy" alt=""/></a>
-                <a href="https://github.com/0xHashstack" rel="noreferrer" target="_blank" className="w-inline-block"><img src="./images/github.svg" loading="lazy" alt="" /></a>
-                <a href="http://hashstack.community" rel="noreferrer" target="_blank" className="w-inline-block"><img src="./images/discord.svg" loading="lazy" alt="" /></a>
-                <a href="https://hashstack.medium.com/" rel="noreferrer" target="_blank" className="w-inline-block"><img src="./images/medium.svg" loading="lazy" alt="" /></a>
-                <a href="https://www.reddit.com/r/Hashstack/" rel="noreferrer" target="_blank" className="w-inline-block"><img src="./images/reddit.svg" loading="lazy" alt="" /></a>
+                <a
+                  href="https://twitter.com/0xhashstack"
+                  rel="noreferrer"
+                  target="_blank"
+                  className="w-inline-block"
+                >
+                  <img
+                    src="./images/Twitter-Icon-Fill.svg"
+                    loading="lazy"
+                    alt=""
+                  />
+                </a>
+                <a
+                  href="https://in.linkedin.com/company/0xhashstack"
+                  rel="noreferrer"
+                  target="_blank"
+                  className="w-inline-block"
+                >
+                  <img
+                    src="./images/Linkedin-Icon-Fill.svg"
+                    loading="lazy"
+                    alt=""
+                  />
+                </a>
+                <a
+                  href="https://github.com/0xHashstack"
+                  rel="noreferrer"
+                  target="_blank"
+                  className="w-inline-block"
+                >
+                  <img src="./images/github.svg" loading="lazy" alt="" />
+                </a>
+                <a
+                  href="http://hashstack.community"
+                  rel="noreferrer"
+                  target="_blank"
+                  className="w-inline-block"
+                >
+                  <img src="./images/discord.svg" loading="lazy" alt="" />
+                </a>
+                <a
+                  href="https://hashstack.medium.com/"
+                  rel="noreferrer"
+                  target="_blank"
+                  className="w-inline-block"
+                >
+                  <img src="./images/medium.svg" loading="lazy" alt="" />
+                </a>
+                <a
+                  href="https://www.reddit.com/r/0xHashstack/"
+                  rel="noreferrer"
+                  target="_blank"
+                  className="w-inline-block"
+                >
+                  <img src="./images/reddit.svg" loading="lazy" alt="" />
+                </a>
               </div>
             </Col>
           </Row>
@@ -25,4 +75,4 @@ const Footer = () => {
   )
 }
 
-export default Footer;
+export default Footer
