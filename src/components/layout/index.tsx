9ce2d4e51597351c16bcd2a5280dfb731dc202fe
Shrutika <shrutika@Shrutikas-MacBook-Pro.local>
2022-04-25 18:12:42 +0530

    update whitelisting to true forever

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -51,7 +51,7 @@ const Layout = props => {
 
           dispatch(changePreloader(true))
           setIsWhiteListedAccountRequested(res.data["whitelist_Requested"])
-          setIsWhiteListedAccount(res.data["isWhiteListed"])
+          setIsWhiteListedAccount(true)
           setCounter(res.data["waitlist_ct"])
           timer = setTimeout(() => {
             dispatch(changePreloader(false))
